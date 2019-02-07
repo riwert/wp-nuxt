7297aaddce44e0505e619c34bb7d80725a510e6c
Riwert <riwert@gmail.com>
2019-02-07 21:06:34 +0100

    move pulse transition to index

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,7 @@
 
 <script>
 export default {
+  transition: 'pulse',
   head() {
     return {
       title: 'Nuxt App',
--- pages/services.vue
+++ pages/services.vue
@@ -10,7 +10,6 @@
 
 <script>
 export default {
-  transition: 'pulse',
   head() {
     return {
       title: 'Services Nuxt App',
